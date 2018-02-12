df2cfa91491f88a7dca77279ec52215cca551490
Luciano Teixeira <luciano.teixeiras@gmail.com>
2018-02-12 21:29:37 -0200

    Commit Aula 01

---
+++ coleta.R
@@ -0,0 +1,14 @@
+# Setar diretorio de trabalho
+setwd("~/GitHub/AluraPipeLineBigData")
+
+#Verificar diretorio de trabalho
+getwd()
+
+#Install packages
+install.packages('httr')
+install.packages('XML')
+
+#Carregar packages
+library(httr)
+library(XML)
+
